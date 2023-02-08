7501e362d1a3b9050ebd10a96743ad602077b348
TarikMaljanovicSEEBA <tarik.maljanovic@seeba.se>
2023-02-09 00:50:53 +0100

    Delivery Service image fixed

--- src/pages/Components/deliveryServices.js
+++ src/pages/Components/deliveryServices.js
@@ -15,9 +15,9 @@ export default function DeliveryServices() {
                 </div>
                 <div class='column'>
                     <div class='columns'>
-                        <div class='column'><div class='notification has-text-centered'><img src={wolt.src} alt='wold'></img></div></div>
-                        <div class='column'><div class='notification has-text-centered'><img src={foodora.src} alt='foodora'></img></div></div>
-                        <div class='column'><div class='notification has-text-centered'><img src={waytogo.src} alt='way to go'></img></div></div>
+                        <a href='https://wolt.com/sv/swe/vasteras/restaurant/hrnet'><div class='column'><div class='notification has-text-centered'><img src={wolt.src} alt='wold'></img></div></div></a>
+                        <a href='https://www.foodora.se/restaurant/wuvx/hornet-vasteras'><div class='column'><div class='notification has-text-centered'><img src={foodora.src} alt='foodora'></img></div></div></a>
+                        <a href='https://waytogo.se/restauranger/mat/hornet-vasteras'><div class='column'><div class='notification has-text-centered'><img src={waytogo.src} alt='way to go'></img></div></div></a>
                     </div>
                 </div>
             </div>
