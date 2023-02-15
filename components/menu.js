7d09cd686abe80d57c959d8a084438b809474a8d
TarikMaljanovicSEEBA <124561218+TarikMaljanovicSEEBA@users.noreply.github.com>
2023-02-15 17:34:12 +0100

    static website

--- components/menu.js
+++ components/menu.js
@@ -1,41 +1,151 @@
 import { render } from 'react-dom'
 import { Inter } from '@next/font/google'
 import styles from 'styles/menu.module.scss'
+import img1 from 'public/images/menu/img1.png'
+import img2 from 'public/images/menu/img2.png'
+import img3 from 'public/images/menu/img3.png'
+import img4 from 'public/images/menu/img4.png'
+import img5 from 'public/images/menu/img5.png'
+import img6 from 'public/images/menu/img6.png'
 
 export default function Menu() {
     return(
         <>
-        <section class="hero is-fullheight">
+        <section class="hero is-fullheight menu-hero">
             <div class="hero-body menu-hero-body">
-                <div class='container menu'>
-                    <div class='menu-section-title'>
-                        <h2 class='back-title'>Meny</h2>
-                        <h3 class='front-title'>Meny</h3>
+                <div class='rows menu-rows'>
+                    <div class='row menu-label'>
+                        <p class='menu-label-back'>Meny</p>
+                        <p class='menu-label-front'>Meny</p>
                     </div>
-                    <div class='menu-section-navbar m-5'>
-                        <div class='menu-section-option-active'>
-                            <p class='navbar-label-active'>Salader</p>
+                    <div class='row menu-navbar'>
+                        <div class='menu-navbar-option-active'>
+                            <p class='menu-option-label'>Salader</p>
                         </div>
-                        <div class='menu-section-option-inactive'>
-                            <p class='navbar-label-inactive'>Pasta</p>
+                        <div class='menu-navbar-option-inactive'>
+                            <p class='menu-option-label'>Pasta</p>
                         </div>
-                        <div class='menu-section-option-inactive'>
-                            <p class='navbar-label-inactive'>Wraps</p>
+                        <div class='menu-navbar-option-inactive'>
+                            <p class='menu-option-label'>Wraps</p>
                         </div>
-                        <div class='menu-section-option-inactive'>
-                            <p class='navbar-label-inactive'>Toast</p>
+                        <div class='menu-navbar-option-inactive'>
+                            <div class='menu-option-label'>Toast</div>
                         </div>
-                        <div class='menu-section-option-inactive'>
-                            <p class='navbar-label-inactive'>Juicer</p>
+                        <div class='menu-navbar-option-inactive'>
+                            <div class='menu-option-label'>Juicer</div>
                         </div>
-                        <div class='menu-section-option-inactive'>
-                            <p class='navbar-label-inactive'>Dryck</p>
+                        <div class='menu-navbar-option-inactive'>
+                            <div class='menu-option-label'>Dryck</div>
                         </div>
-                        <hr class='navbar-divider'/>
                     </div>
-                    <h2 class='menu-section-category-label'>Category</h2>
-                    <div class='columns menu-section-body'>
-                       
+                    <div class='row divider'>
+                        <hr class='dopdown divider'/>
+                    </div>
+                    <div class='row category'>
+                        <p class='category-label'>Category</p>
+                    </div>
+                    <div class='row menu-body'>
+                        <div class='columns menu-row'>
+                            <div class='column item-wimg'>
+                                <img src={img1.src} class='menu-item-img'></img>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>In fusce eget.</p>
+                                    <p class='item-description'>Facilisis purus eu velit vitae tristique pulvinar in aliquam. </p>
+                                    <p class='item-price'>160 SEK</p>
+                                    <div class='item-tags'>
+                                        <p class='item-tag'>Laktosfritt</p>
+                                        <p class='item-tag'>Vetefritt</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='column item-noimg'>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>Mi velit.</p>
+                                    <p class='item-description'>Dictum sed scelerisque vitae scelerisque enim, dictumst. Ornare tempor duis tortor id convallis proin. Viverra risus quis convallis volutpat diam sed sit.</p>
+                                    <p class='item-price'>160 SEK</p>
+                                    <div class='item-tags'>
+                                        <p class='item-tag'>Laktosfritt</p>
+                                        <p class='item-tag'>Vetefritt</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='column item-wimg'>
+                                <img src={img5.src} class='menu-item-img'></img>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>In fusce eget.</p>
+                                    <p class='item-description'>Facilisis purus eu velit vitae tristique pulvinar in aliquam. </p>
+                                    <p class='item-price'>160 SEK</p>
+                                    <div class='item-tags'>
+                                        <p class='item-tag'>Laktosfritt</p>
+                                        <p class='item-tag'>Vetefritt</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='columns menu-row'>
+                            <div class='column item-wimg'>
+                                <img src={img2.src} class='menu-item-img'></img>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>In fusce eget.</p>
+                                    <p class='item-description'>Facilisis purus eu velit vitae tristique pulvinar in aliquam. </p>
+                                </div>
+                            </div>
+                            <div class='column item-wimg'>
+                                <img src={img4.src} class='menu-item-img'></img>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>In fusce eget.</p>
+                                    <p class='item-description'>Facilisis purus eu velit vitae tristique pulvinar in aliquam. </p>
+                                    <p class='item-price'>160 SEK</p>
+                                    <div class='item-tags'>
+                                        <p class='item-tag'>Laktosfritt</p>
+                                        <p class='item-tag'>Vetefritt</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='column item-wimg'>
+                                <img src={img4.src} class='menu-item-img'></img>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>In fusce eget.</p>
+                                    <p class='item-description'>Facilisis purus eu velit vitae tristique pulvinar in aliquam. </p>
+                                    <p class='item-price'>160 SEK</p>
+                                    <div class='item-tags'>
+                                        <p class='item-tag'>Laktosfritt</p>
+                                        <p class='item-tag'>Vetefritt</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='columns menu-row'>
+                            <div class='column item-wimg'>
+                                <img src={img3.src} class='menu-item-img'></img>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>In fusce eget.</p>
+                                    <p class='item-description'>Facilisis purus eu velit vitae tristique pulvinar in aliquam. </p>
+                                </div>
+                            </div>
+                            <div class='column item-noimg'>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>Mi velit.</p>
+                                    <p class='item-description'>Dictum sed scelerisque vitae scelerisque enim, dictumst. Ornare tempor duis tortor id convallis proin. Viverra risus quis convallis volutpat diam sed sit.</p>
+                                    <p class='item-price'>160 SEK</p>
+                                    <div class='item-tags'>
+                                        <p class='item-tag'>Laktosfritt</p>
+                                        <p class='item-tag'>Vetefritt</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='column item-noimg'>
+                                <div class='menu-item-description'>
+                                    <p class='item-title'>Mi velit.</p>
+                                    <p class='item-description'>Dictum sed scelerisque vitae scelerisque enim, dictumst. Ornare tempor duis tortor id convallis proin. Viverra risus quis convallis volutpat diam sed sit.</p>
+                                    <p class='item-price'>160 SEK</p>
+                                    <div class='item-tags'>
+                                        <p class='item-tag'>Laktosfritt</p>
+                                        <p class='item-tag'>Vetefritt</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
