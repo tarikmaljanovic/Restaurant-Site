cd0e3b91c8985f2f82e6a0553aa0e0c84ee44dfb
TarikMaljanovicSEEBA <124561218+TarikMaljanovicSEEBA@users.noreply.github.com>
2023-02-18 03:02:07 +0100

    navbar elements change on scroll

--- components/navBar.js
+++ components/navBar.js
@@ -2,10 +2,26 @@ import { Inter } from '@next/font/google'
 import styles from 'styles/navbar.module.scss'
 import { render } from 'react-dom'
 import { Link } from 'react-scroll'
+import { useState, useEffect } from 'react'
 import logo from 'public/images/Logo.png'
 import basket from 'public/icons/basket.png'
 
 export default function NavBar() {
+    const [hem, setHem] = useState(false);
+    const [meny, setMeny] = useState(false);
+    const [omoss, setOmoss] = useState(false);
+    const [kontakt, setKontakt] = useState(false);
+    
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setHem(window.scrollY >= 0 && window.scrollY <= 900);
+            setMeny(window.scrollY > 900 && window.scrollY <= 2200);
+            setOmoss(window.scrollY > 2200 && window.scrollY <= 2700);
+            setKontakt(window.scrollY > 2700);
+        })
+    },[]);
+    
+
     return (
         <>
         <div class='header navbar-header'>
@@ -17,16 +33,16 @@ export default function NavBar() {
                     <div class='column is-4 navbar-options'>
                         <ul class='options'>
                             <Link to='titleBanner-hero' smooth={true} offset={0} duration={500}>
-                                <li class='hem'>HEM</li>
+                                <li class={`hem ${hem ? '-active' : '-inactive'}`}>HEM</li>
                             </Link>
                             <Link to='menu-section' smooth={true} offset={0} duration={500}>
-                                <li class='meny'>MENY</li>
+                                <li class={`meny ${meny ? '-active' : '-inactive' }`}>MENY</li>
                             </Link>
                             <Link to='aboutus-section' smooth={true} offset={-50} duration={500}>
-                                <li class='om-oss'>OM OSS</li>
+                                <li class={`om-oss ${omoss ? '-active' : '-inactive'}`}>OM OSS</li>
                             </Link>
                             <Link to='info-section' smooth={true} offset={0} duration={500}>
-                                <li class='kontakt'>KONTAKT</li>
+                                <li class={`kontakt ${kontakt ? '-active' : '-inactive'}`}>KONTAKT</li>
                             </Link>
                         </ul>
                     </div> 
