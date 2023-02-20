8f5067ef3a480d24873113e7c202e9f6ba7b1a79
MelikaH <57252386+MelikaH-seeba@users.noreply.github.com>
2023-02-20 10:30:46 +0100

    takeaway

--- components/takeawayBanner.js
+++ components/takeawayBanner.js
@@ -9,9 +9,10 @@ export default function TakeawayBanner() {
         <>
         <section class="section is-small takeawayBanner-section p-0">
                 <div class='container takeawayBanner-container is-fluid'>
+                <p class='takeaway-label m-1'>TAKEAWAY?</p>
                     <h2 class='takeaway-title m-2'>Beställ takeaway från oss!</h2>
                     <p class='takeaway-subtitle m-2'>Välj vad du vill köpa och när det passar för dig att hämta upp - vi gör resten.</p>
-                    <a href='https://www.foodora.se/en/restaurant/wuvx/hornet-vasteras'>
+                    <a href='https://www.foodora.se/en/restaurant/wuvx/hornet-vasteras' target="blank">
                         <button class='button takeaway-button m-2'>
                             <img src={basket.src} alt='basket'></img>
                             <p class='button-label'>BESTÄLL TAKEAWAY</p>
